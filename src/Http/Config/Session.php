6e28a03b5d67da41a89f6594a579c14780203bb3
James Deathe <james.deathe@gmail.com>
2018-04-06 10:34:49 +0100

    ISSUE 64: Adds session.use_strict_mode to app's PHP configuration.

--- src/Http/Config/Session.php
+++ src/Http/Config/Session.php
@@ -1,19 +1,5 @@
 <?php
 
-// Require strict mode if available
-if (
-    version_compare(
-        PHP_VERSION,
-        '5.5.2',
-        '>='
-    )
-) {
-    ini_set(
-        'session.use_strict_mode',
-        '1'
-    );
-}
-
 // Required settings for multiple Memcached session store nodes
 if (
     ini_get(
