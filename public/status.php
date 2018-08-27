b0881832db098a53f0868ac4521d1ac271b2b0ab
James Deathe <james.deathe@gmail.com>
2018-08-27 11:28:19 +0100

    ISSUE 88: Adds basic server status code checks.

--- public/status.php
+++ public/status.php
@@ -25,9 +25,35 @@ header(
     true
 );
 
-Html::printEncoded(
-    $viewSettings->get(
-        'status_success',
-        'OK'
+$statusCode = function_exists(
+        'http_response_code'
     )
-);
+    ? (int) http_response_code()
+    : (int) $_SERVER['REDIRECT_STATUS']
+;
+if (
+    $statusCode === 503
+) {
+    Html::printEncoded(
+        $viewSettings->get(
+            'status_unavailable',
+            'Service Unavailable'
+        )
+    );
+} elseif (
+    $statusCode >= 500
+) {
+    Html::printEncoded(
+        $viewSettings->get(
+            'status_error',
+            'Internal Server Error'
+        )
+    );
+} else {
+    Html::printEncoded(
+        $viewSettings->get(
+            'status_success',
+            'OK'
+        )
+    );
+}
